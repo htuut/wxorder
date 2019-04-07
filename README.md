d95e22acf3576b934ba6aa6f6f1077c27ea397a9
fangwu <273569757@qq.com>
2019-04-07 18:20:10 +0800

    查询订单列表接口

--- README.md
+++ README.md
@@ -28,3 +28,48 @@
     }
 }
 ```
+# 订单相关
+
+#### 1、查询列表
+- ##### 请求地址 get：http://localhost:8080/htuut/buyer/order/list
+- ##### 请求参数
+
+```
+openid
+page
+size
+```
+- ##### 返回结果
+```
+{
+    "code": "1",
+    "msg": "成功",
+    "data": [
+        {
+            "orderId": "1554631480163872881",
+            "buyerName": "jack",
+            "buyerPhone": "17688888888",
+            "buyerAddress": "shenzhen",
+            "buyerOpenid": "fuiosdhfahkshfka",
+            "orderAmount": 20,
+            "orderStatus": 0,
+            "payStatus": 0,
+            "createTime": 1554602522000,
+            "updateTime": 1554602522000,
+            "detailList": null
+        },
+        {
+            "orderId": "1554631489955158995",
+            "buyerName": "jack",
+            "buyerPhone": "17688888888",
+            "buyerAddress": "shenzhen",
+            "buyerOpenid": "fuiosdhfahkshfka",
+            "orderAmount": 20,
+            "orderStatus": 0,
+            "payStatus": 0,
+            "createTime": 1554602531000,
+            "updateTime": 1554602531000,
+            "detailList": null
+        }
+    ]
+}
--- wxorder/src/main/java/com/htuut/controller/BuyerOrderController.java
+++ wxorder/src/main/java/com/htuut/controller/BuyerOrderController.java
@@ -10,14 +10,16 @@ import com.htuut.utils.ResultVOUtil;
 import com.htuut.vo.ResultVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -55,6 +57,22 @@ public class BuyerOrderController {
     }
 
     //订单列表
+    @GetMapping("/list")
+    public ResultVO<List<OrderDTO>> list(@RequestParam("openid") String openid,
+                                         @RequestParam(value = "page", defaultValue = "0") Integer page,
+                                         @RequestParam(value = "size", defaultValue = "10") Integer size) {
+
+        if (StringUtils.isEmpty(openid)) {
+            log.error("【查询订单列表】openid 为空");
+            throw new SellException(ResultEnum.PARAM_ERROR);
+        }
+
+        PageRequest pageRequest = new PageRequest(page, size);
+        Page<OrderDTO> orderDTOPage = orderService.findList(openid, pageRequest);
+        return ResultVOUtil.success(orderDTOPage.getContent());
+
+    }
+
 
     //订单详情
 
